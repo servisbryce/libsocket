2bbc7d43965b8336cb8fa18aea87352a3b4aa373
servisbryce <bryceservis@bryceserv.is>
2025-03-24 22:04:37 -0500

    Quick patch.

--- src/helpers/sockaddr.c
+++ src/helpers/sockaddr.c
@@ -34,7 +34,6 @@ int create_sockaddr(char *address, struct sockaddr **sockaddr) {
 
     /* Construct the socket address and pass the data to the user. */
     struct sockaddr *result = response->ai_addr;
-    result->sa_family = AF_INET;
     *sockaddr = result;
 
     /* Return our memory back to the user and declare success. */
