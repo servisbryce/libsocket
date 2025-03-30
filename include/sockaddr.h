acb3fb867ae9915c4004115df86febe64f53cea8
servisbryce <bryceservis@bryceserv.is>
2025-03-30 18:35:39 -0500

    Updated sockaddr.h

---
+++ include/sockaddr.h
@@ -0,0 +1,9 @@
+#ifndef SOCKADDR_H_
+#define SOCKADDR_H_
+
+#include <stdint.h>
+
+struct sockaddr *create_sockaddr(void *address, uint16_t port);
+void destroy_sockaddr(struct sockaddr *sockaddr);
+
+#endif
