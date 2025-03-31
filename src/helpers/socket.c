031e0af1ff19d412f5b16cd3f9f622e42d299a81
servisbryce <bryceservis@bryceserv.is>
2025-03-30 19:12:44 -0500

    Improve error handling.

--- src/helpers/socket.c
+++ src/helpers/socket.c
@@ -20,20 +20,25 @@ int create_socket(struct sockaddr *sockaddr) {
     int socket_options = SO_REUSEADDR | SO_REUSEPORT;
     if (setsockopt(socket_file_descriptor, SOL_SOCKET, socket_options, &socket_options, sizeof(socket_options)) == -1) {
 
+        close(socket_file_descriptor);
         return -1;
 
     }
-    
+
     if (bind(socket_file_descriptor, sockaddr, sockaddr_length) == -1) {
 
+        close(socket_file_descriptor);
         return -1;
 
     }
 
     if (listen(socket_file_descriptor, 4096) == -1) {
 
+        close(socket_file_descriptor);
         return -1;
 
     }
+
+    return socket_file_descriptor;
 
 }
